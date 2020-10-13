1e492e989498a3c1ba81200ed401de542f6b8645
stefanbpeterson <stefanbpeterson@gmail.com>
2020-10-13 15:00:57 -0600

    changing text message

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.post('/message', function(req, res, next) {
   client.messages.create({
     to: req.body.to,
     from: TWILIO_PHONE_NUMBER,
-    body: 'Ren is a nerd'
+    body: 'Eva is cool!'
   }).then(function(message) {
     // When we get a response from Twilio, respond to the HTTP POST request
     res.send('Message is inbound!');
